545379f2c860e5d5181bf4ccead67fd9ec43c465
fall lewis <fallewi@gmail.com>
2023-03-31 12:27:05 +0200

    correction du code

--- tp-6/dev/main.tf
+++ tp-6/dev/main.tf
@@ -1,18 +1,18 @@
 module "network" {
   source = "../modules/network"
-    }
+}
 
 module "firewall" {
   source = "../modules/firewall"
   vpc_id = module.network.output_vpc_id
-      }
+}
 
 module "instance" {
-  source = "../modules/instance"
-    vpc_id = module.network.output_subnet_id
-          }
+  source    = "../modules/instance"
+  subnet_id = module.network.output_subnet_id
+}
 
 
 
 
-    
+
